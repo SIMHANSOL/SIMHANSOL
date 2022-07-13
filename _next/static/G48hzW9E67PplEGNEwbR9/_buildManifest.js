50e4fc6feb800a06dbc60becfedde12e8f1c0707
SIMHANSOL <zuluy@naver.com>
2022-07-13 22:43:01 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/G48hzW9E67PplEGNEwbR9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/31b2d10f9ad352ff.css","static/chunks/pages/index-61b309e18aeba9ae.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
